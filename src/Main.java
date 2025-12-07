077463a28f65a699200f5af144ace872e0392582
Antonio Augusto da Fonseca <anfonseca44@gmail.com>
2025-12-07 19:04:14 -0300

    -- Conclusão Fase 1 (removendo espaços em branco).

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ ANTONIO AUGUSTO DA FONSECA - 25046981-5
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Desenvolvido por Antonio Augusto da Fonseca - 25046981-5");
         Curso curso = new Curso("Análise e Desenvolvimento de Sistemas", "001", 2400);
         Aluno aluno = new Aluno("Maria Silva", "2025001", curso);
         Professor professor = new Professor("Dr. João", "Matemática", "REG1234");
